58cb2a0266798ee8a7afe0bc95b43be89a67c09d
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-04-12 12:44:37 +0900

    feat: Add fetch repository class `DayWeatherRepository`

--- lib/feature/day_weather/repository.dart
+++ lib/feature/day_weather/repository.dart
@@ -1,1 +1,12 @@
+import 'package:flutter_training/common/utils/extensions/enum.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+
 enum WeatherType { sunny, rainy, cloudy }
+
+class DayWeatherRepository {
+  final _client = YumemiWeather();
+  WeatherType? fetch() {
+    final response = _client.fetchSimpleWeather();
+    return WeatherType.values.byNameOrNull(response);
+  }
+}
