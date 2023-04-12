b81148ca201e603a217b51798294d72c38bcc908
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-04-12 12:50:51 +0900

    update: Place `WeatherIcon` to `WeatherInfo`

--- lib/feature/day_weather/components/weather_info.dart
+++ lib/feature/day_weather/components/weather_info.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training/feature/day_weather/components/temperature_text.dart';
+import 'package:flutter_training/feature/day_weather/components/weather_icon.dart';
+import 'package:flutter_training/feature/day_weather/repository.dart';
 
 class WeatherInfo extends StatelessWidget {
   const WeatherInfo({
     required this.highTemperature,
     required this.lowTemperature,
+    required this.weatherType,
     super.key,
   });
   final int? highTemperature;
   final int? lowTemperature;
+  final WeatherType? weatherType;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const AspectRatio(aspectRatio: 1, child: Placeholder()),
+        AspectRatio(
+              aspectRatio: 1,
+              child: weatherType == null
+                  ? const Placeholder()
+                  : WeatherIcon(weatherType: weatherType!),
+            ),
         const SizedBox(
           height: 16,
         ),
