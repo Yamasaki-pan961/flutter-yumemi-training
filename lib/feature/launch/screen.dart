6c64550c1cf317ef0f6294868a651cda9408b0e5
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-04-14 14:11:57 +0900

    feat: add Launch Screen

---
+++ lib/feature/launch/screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LaunchScreen extends StatefulWidget {
+  const LaunchScreen({super.key});
+
+  @override
+  State<LaunchScreen> createState() => LaunchScreenState();
+}
+
+class LaunchScreenState extends State<LaunchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
