e304a5f62ea2d2a94066611c93d8f4f62b875d71
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-04-17 14:19:09 +0900

    feat: add `_delayedNavigation` in `LaunchScreenState`

--- lib/feature/launch/screen.dart
+++ lib/feature/launch/screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_training/feature/day_weather/screen.dart';
 
 class LaunchScreen extends StatefulWidget {
   const LaunchScreen({super.key});
@@ -8,6 +9,26 @@ class LaunchScreen extends StatefulWidget {
 }
 
 class LaunchScreenState extends State<LaunchScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.endOfFrame.then((_) => _delayedNavigation());
+  }
+
+  Future<void> _delayedNavigation() async {
+    await Future<void>.delayed(const Duration(milliseconds: 500));
+
+    if (!mounted) {
+      return;
+    }
+    final pageRoute = MaterialPageRoute<String>(
+      builder: (context) => const DayWeatherScreen(),
+    );
+
+    await Navigator.of(context).push(pageRoute);
+    await _delayedNavigation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
