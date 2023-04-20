c39dfa0dbcf400844514e8922bdeef7fb3fffbbd
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-04-20 12:27:37 +0900

    feat: Add `CallbackAfterBuildMixin`

---
+++ lib/common/utils/mixin/callback_after_build.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+/// A mixin that can be used with a [StatefulWidget] to register a callback.
+/// function [callbackAfterBuild] to be called after the widget is built.
+/// To use this mixin, create a class that extends [StatefulWidget] and mixes in.
+/// this class. Then, implement the [callbackAfterBuild] function to perform any.
+mixin CallbackAfterBuildMixin<T extends StatefulWidget> on State<T> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.endOfFrame.then((_) => callbackAfterBuild());
+  }
+
+  void callbackAfterBuild();
+}
