9887e479f56bf7c7ecc293a081aa2242cf1a9187
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-04-20 12:29:39 +0900

    refactor: Change to use `CallbackAfterBuildMixin` in `LaunchScreen`

--- lib/feature/launch/screen.dart
+++ lib/feature/launch/screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_training/common/utils/mixin/callback_after_build.dart';
 import 'package:flutter_training/feature/day_weather/screen.dart';
 
 class LaunchScreen extends StatefulWidget {
@@ -8,13 +9,8 @@ class LaunchScreen extends StatefulWidget {
   State<LaunchScreen> createState() => LaunchScreenState();
 }
 
-class LaunchScreenState extends State<LaunchScreen> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.endOfFrame.then((_) => _delayedNavigation());
-  }
-
+class LaunchScreenState extends State<LaunchScreen>
+    with CallbackAfterBuildMixin {
   Future<void> _delayedNavigation() async {
     await Future<void>.delayed(const Duration(milliseconds: 500));
 
@@ -27,6 +23,11 @@ class LaunchScreenState extends State<LaunchScreen> {
 
     await Navigator.of(context).push(pageRoute);
     await _delayedNavigation();
+  }
+
+  @override
+  void callbackAfterBuild() {
+    _delayedNavigation();
   }
 
   @override
