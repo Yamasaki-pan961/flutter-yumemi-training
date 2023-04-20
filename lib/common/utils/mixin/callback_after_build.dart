cb4c7bdb1b2e96a5e8a2f9fa262fd3cabc2c2822
Yamasaki-pan961 <54800851+Yamasaki-pan961@users.noreply.github.com>
2023-04-20 15:12:05 +0900

    docs: fix description
    Co-authored-by: blendthink <32213113+blendthink@users.noreply.github.com>

--- lib/common/utils/mixin/callback_after_build.dart
+++ lib/common/utils/mixin/callback_after_build.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 ///
 /// function [callbackAfterBuild] to be called after the widget is built.
 /// To use this mixin, create a class
-///  that extends [StatefulWidget] and mixes in.
-/// this class. Then, implement the [callbackAfterBuild]
+///  that extends [StatefulWidget] and mixes in
+/// that class. Then, implement the [callbackAfterBuild]
 ///  function to perform any.
 mixin CallbackAfterBuildMixin<T extends StatefulWidget> on State<T> {
   @override
