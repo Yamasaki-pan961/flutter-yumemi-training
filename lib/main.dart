17a134b596e7f4bf04aa531f96a1fbb239dd6f6f
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-04-26 16:51:57 +0900

    feat: Set logger config for release build

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_training/app.dart';
+import 'package:simple_logger/simple_logger.dart';
 
 void main() {
+  const logLevel = kReleaseMode ? Level.OFF : Level.FINE;
+  SimpleLogger().setLevel(
+    logLevel,
+    includeCallerInfo: !kReleaseMode,
+  );
   runApp(const MyApp());
 }
