4cd01c63a286e42e2f8b4c5c7f07909effc74805
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-05-12 16:39:37 +0900

    refactor(`DayWeather`): Change the folder to view

--- lib/feature/day_weather/presentation/view/day_weather_screen.dart
+++ lib/feature/day_weather/presentation/view/day_weather_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training/common/domain/entities/weather.dart';
-import 'package:flutter_training/feature/day_weather/components/weather_info.dart';
+import 'package:flutter_training/feature/day_weather/presentation/view/components/weather_info.dart';
 import 'package:flutter_training/feature/day_weather/repository.dart';
 
 class DayWeatherScreen extends StatefulWidget {
