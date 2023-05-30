ec24722d723ca6f6bf2ef30b5af4220825929720
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-05-30 17:21:40 +0900

    test(dayWeatherProvider): Add the number of calls to fetch() and use case

--- test/feature/day_weather/presentation/presenter/day_weather_provider_test.dart
+++ test/feature/day_weather/presentation/presenter/day_weather_provider_test.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_training/common/domain/entities/weather.dart';
+import 'package:flutter_training/common/utils/result.dart';
 import 'package:flutter_training/feature/day_weather/domain/use_case/fetch_day_weather_use_case.dart';
 import 'package:flutter_training/feature/day_weather/presentation/presenter/day_weather_provider.dart';
 import 'package:mockito/annotations.dart';
@@ -29,4 +32,74 @@ class RiverpodTestTools {
 
 @GenerateMocks([FetchDayWeatherUseCase])
 void main() {
+  group(
+    'Test the number of calls to fetch() and use case',
+    () {
+      // Arrange
+      final resultForMatcher = Result<Weather, String>.success(
+        Weather(
+          weatherCondition: WeatherCondition.rainy,
+          maxTemperature: 20,
+          minTemperature: 10,
+          date: DateTime(2023),
+        ),
+      );
+      final useCaseMock = MockFetchDayWeatherUseCase();
+      when(useCaseMock()).thenReturn(resultForMatcher);
+
+      test('0 calls to use case when 0 calls to fetch()', () {
+        // Arrange
+        final riverpodTestTool = RiverpodTestTools(useCaseMock: useCaseMock);
+
+        // Act
+        // 0 calls to fetch()
+
+        // Assert
+        verifyNever(
+          riverpodTestTool.container
+              .read(fetchDayWeatherUseCaseProvider)
+              .call(),
+        );
+      });
+
+      test(
+        '1 calls to use case when 1 calls to fetch()',
+        () {
+          // Arrange
+          final riverpodTestTool = RiverpodTestTools(useCaseMock: useCaseMock);
+
+          // Act
+          riverpodTestTool.container
+              .read(dayWeatherProvider.notifier)
+              .fetchWeather();
+
+          // Assert
+          verify(
+            riverpodTestTool.container
+                .read(fetchDayWeatherUseCaseProvider)
+                .call(),
+          ).called(1);
+        },
+      );
+
+      test(
+        '2 calls to use case when 2 calls to fetch()',
+        () {
+          // Arrange
+          final riverpodTestTool = RiverpodTestTools(useCaseMock: useCaseMock);
+
+          // Act
+          riverpodTestTool.container
+              .read(dayWeatherProvider.notifier)
+              .fetchWeather();
+          riverpodTestTool.container
+              .read(dayWeatherProvider.notifier)
+              .fetchWeather();
+
+          // Assert
+          verify(useCaseMock()).called(2);
+        },
+      );
+    },
+  );
 }
