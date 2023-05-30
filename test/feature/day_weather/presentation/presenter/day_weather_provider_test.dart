1ee3e5f985c225164afa5909645f121ecfaac854
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-05-30 17:21:40 +0900

    test(day_weather_provider): Classified boilerplate for testing

--- test/feature/day_weather/presentation/presenter/day_weather_provider_test.dart
+++ test/feature/day_weather/presentation/presenter/day_weather_provider_test.dart
@@ -1,7 +1,32 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_training/feature/day_weather/domain/use_case/fetch_day_weather_use_case.dart';
+import 'package:flutter_training/feature/day_weather/presentation/presenter/day_weather_provider.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
 import 'day_weather_provider_test.mocks.dart';
+
+class Listener<T> extends Mock {
+  void call(T? previous, T value);
+}
+
+/// Initialize and hold Riverpod containers and listeners
+class RiverpodTestTools {
+  RiverpodTestTools({required FetchDayWeatherUseCase useCaseMock})
+      : container = ProviderContainer(
+          overrides: [
+            fetchDayWeatherUseCaseProvider.overrideWithValue(useCaseMock)
+          ],
+        ) {
+    addTearDown(container.dispose);
+
+    container.listen<Weather?>(dayWeatherProvider, listener);
+  }
+
+  final ProviderContainer container;
+  final listener = Listener<Weather?>();
+}
+
 @GenerateMocks([FetchDayWeatherUseCase])
 void main() {
 }
