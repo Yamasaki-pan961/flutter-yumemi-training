7fea8c698288ce2e889fb651e38a031f4a767a82
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-05-30 17:21:40 +0900

    test(provider): Add dayWeatherProvider test with mock

---
+++ test/feature/day_weather/presentation/presenter/day_weather_provider_test.dart
@@ -0,0 +1,7 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'day_weather_provider_test.mocks.dart';
+@GenerateMocks([FetchDayWeatherUseCase])
+void main() {
+}
