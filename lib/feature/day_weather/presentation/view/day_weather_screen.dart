f95a1772d3609d18770a10d44c7dd438abc4bdd3
Yamasaki-pan961 <atusi08250522@gmail.com>
2023-07-24 17:49:05 +0900

    fix: LoadingSpinner logic

--- lib/feature/day_weather/presentation/view/day_weather_screen.dart
+++ lib/feature/day_weather/presentation/view/day_weather_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_training/common/utils/api_call/dio_exception.dart';
 import 'package:flutter_training/feature/day_weather/presentation/presenter/day_weather_provider.dart';
 import 'package:flutter_training/feature/day_weather/presentation/view/components/weather_info.dart';
 
@@ -12,29 +13,22 @@ class DayWeatherScreen extends ConsumerWidget {
   @visibleForTesting
   static final closeButtonKey = UniqueKey();
 
+  @visibleForTesting
+  static final loadingSpinnerKey = UniqueKey();
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    ref.listen(dayWeatherProvider, (prev, current) {
-      if (prev?.isRefreshing ?? false) {
-        Navigator.of(context).pop();
-      }
-      if (current.isRefreshing) {
+    ref.listen(dayWeatherProvider, (_, current) {
+      if (current.isLoading) {
         showDialog<void>(
-          context: context,
-          barrierDismissible: false,
-          builder: (context) => const Center(
-            child: CircularProgressIndicator(),
-          ),
-        );
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => _LoadingDialog(key: loadingSpinnerKey));
       }
     });
+
     void onClose() => Navigator.of(context).pop();
-    void onReload() => ref.read(dayWeatherProvider.notifier).fetchWeather(
-          onError: (errorMessage) => showDialog<void>(
-            context: context,
-            builder: (context) => _FetchErrorDialog(errorMessage: errorMessage),
-          ),
-        );
+    void onReload() => ref.read(dayWeatherProvider.notifier).fetchWeather();
 
     return Scaffold(
       body: Center(
@@ -82,10 +76,13 @@ class DayWeatherScreen extends ConsumerWidget {
 }
 
 class _FetchErrorDialog extends StatelessWidget {
-  const _FetchErrorDialog({required this.errorMessage});
-  final String errorMessage;
+  const _FetchErrorDialog({required this.error});
+  final Object error;
   @override
   Widget build(BuildContext context) {
+    final errorMessage = error is DioException
+        ? getDioExceptionMessage(error as DioException)
+        : '不明なアプリケーションエラーが発生しました';
     return AlertDialog(
       title: const Text('天気取得エラー'),
       content: Text(errorMessage),
@@ -95,6 +92,34 @@ class _FetchErrorDialog extends StatelessWidget {
           child: const Text('OK'),
         )
       ],
+    );
+  }
+}
+
+class _LoadingDialog extends ConsumerWidget {
+  const _LoadingDialog({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(dayWeatherProvider, (_, current) {
+      print(current);
+      if (!current.isLoading) {
+        Navigator.of(context).pop();
+      }
+      current.maybeWhen(
+        error: (asyncError, __) {
+          showDialog<void>(
+            context: context,
+            builder: (context) => _FetchErrorDialog(
+              error: asyncError,
+            ),
+          );
+        },
+        orElse: () {},
+      );
+    });
+    return Center(
+      child: const CircularProgressIndicator(),
     );
   }
 }
